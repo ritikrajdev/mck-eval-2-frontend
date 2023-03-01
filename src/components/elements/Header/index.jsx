ae7d25136b0a281912d3011f248f0b924e788c53
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-03-01 15:31:25 +0530

    feat: redirect to home on main icon click

--- src/components/elements/Header/index.jsx
+++ src/components/elements/Header/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { APP_NAME } from '../../../constants';
+import { HOME_ROUTE } from '../../../constants/routes';
 
 import './Header.css';
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header className='body-padding'>
-      <h1>{APP_NAME.toUpperCase()}</h1>
+      <h1>
+        <span onClick={() => navigate(HOME_ROUTE)}>
+          {APP_NAME.toUpperCase()}
+        </span>
+      </h1>
     </header>
   );
 }
